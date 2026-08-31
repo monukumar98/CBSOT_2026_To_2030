2a1df659ee51323a9fee36b18eefaf0664371063
monukumar98 <monu012as@gmail.com>
2026-08-31 16:56:22 +0530

    Loops Pattern

---
+++ Lec2/Table_7.java
@@ -0,0 +1,16 @@
+package Lec2;
+
+public class Table_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t = 7;// user input
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(t * i);
+			i++;
+		}
+
+	}
+
+}
